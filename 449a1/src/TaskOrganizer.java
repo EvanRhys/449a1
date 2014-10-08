f8bc4e5eeec001749b42486c31c1fd5a973d5f8a
erdkalyn <evanrhyskalynchuk@gmail.com>
2014-10-08 11:43:08 -0600

    Created Task Organizer Class Moved Methods and attributes from driver to the Task Organizer Class

---
+++ 449a1/src/TaskOrganizer.java
@@ -0,0 +1,280 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class TaskOrganizer {
+	private int [][] linePen;
+	private Node [] tooNearPen;
+	private Node [] forcedPart;
+	private Node [] forbiddenM;
+	private Node [] tooNearTask;
+	private final String input = "ABCDEFGH";
+	private final char empty = 'X';
+	private HardConstraints HC;
+	private SoftConstraints SC;
+	private Tree sTree;
+	private String inputFile;
+	private String outputFile;
+	
+	public TaskOrganizer(){}
+	public TaskOrganizer(String inputFile, String outputFile){
+		linePen = new int[8][8];
+		tooNearPen = new Node[8];
+		forcedPart = new Node[8];
+		forbiddenM = new Node[8];
+		tooNearTask = new Node[8];
+		this.inputFile = inputFile;
+		this.outputFile = outputFile;
+		
+	}
+	
+	public void Start() throws IOException{
+		String line;
+		String output;
+		int score;
+		int keyWordCount = 0;
+		FileReader fr;
+		BufferedReader br;
+		try {
+			fr = new FileReader(inputFile);
+			br = new BufferedReader(fr);
+			while((line = br.readLine()) != null){
+				if (line.equalsIgnoreCase("forced partial assignment:")){
+					forcedPartialAssign(br);
+					keyWordCount++;					
+				}
+				else if (line.equalsIgnoreCase("forbidden machine:")){
+					forbiddenMachine(br);
+					keyWordCount++;					
+				}
+				else if (line.equalsIgnoreCase("too-near tasks:")){
+					tooNearTask(br);
+					keyWordCount++;					
+				}
+				else if (line.equalsIgnoreCase("machine penalties:")){
+					machinePenalties(br);
+					keyWordCount++;					
+				}
+				else if (line.equalsIgnoreCase("too-near penalities")){
+					tooNearPen(br);
+					keyWordCount++;
+				}
+				else if(line.equalsIgnoreCase("Name:")){//added to handle when name title is read, garbage
+					line = br.readLine();//added to test name, garbage
+				}
+				else if(line.startsWith(" "));//added to handle line with spaces, garbage
+				else if(line.length() == 0);//added to handle blank line, garbage
+				else
+					throw new ParsingInputException();//added to throw error when garbage is read, garbage
+			}
+			fr.close();
+			HC = new HardConstraints(forcedPart, forbiddenM , tooNearTask);
+			SC = new SoftConstraints(tooNearPen, linePen);
+			sTree = new Tree(HC, SC);
+			if (keyWordCount != 5)
+				throw new ParsingInputException();
+			sTree.searchR(input, "", 1, 0, empty);
+			output = sTree.getBestString();
+			score = sTree.getBestScore();
+			if (!output.equalsIgnoreCase("No valid solution possible!"))
+				output = "Solution " + output + "; Quality:" + score;
+			writeOutput(output, outputFile);
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		} catch (PartialAssignmentException e){
+			output = "partial assignment error";
+			writeOutput(output, outputFile);
+		} catch (ParsingInputException e){
+			output = "Error while parsing input file";
+			writeOutput(output, outputFile);
+		} catch (InvalidInputException e){
+			output = "invalid machine/task";
+			writeOutput(output, outputFile);
+		} catch (MachinePenException e){
+			output = "machine penalty error";
+			writeOutput(output, outputFile);
+		} catch (InvalidPenException e){
+			output = "invalid penalty";
+			writeOutput(output, outputFile);
+		} catch (InvalidTaskException e){
+			output = "invalid task";
+			writeOutput(output, outputFile);
+		}
+		
+	}
+	/*******
+	 * @param br
+	 * @throws IOException
+	 * @throws PartialAssignmentException
+	 */
+	private void forcedPartialAssign(BufferedReader br) throws IOException, PartialAssignmentException, InvalidInputException{
+		String compare = input;
+		String line;
+		String [] temp;
+		CharSequence seq;
+		char c;
+		int i;
+		while(((line = br.readLine()).length() > 0) && ((line.charAt(0)) != ' ')){
+			c = line.charAt(3);
+			i = ((line.charAt(1) - 49)); //Changed to calculate number differently, wrongmachine
+			if(i >= 8)	//added to check if number if greater then 8
+				throw new InvalidInputException();
+			seq = Character.toString((c));
+			if ((forcedPart[i] == null) && (compare.contains(seq)))
+				forcedPart[i] = new forcedNode(line.charAt(1), line.charAt(3));
+			else{
+				if ((c >= 65) && (c <= 72))
+					throw new PartialAssignmentException();
+				else 
+					throw new InvalidInputException();				
+			}
+			temp = compare.split(Character.toString(c));
+			if (temp.length > 1){
+				compare = temp[0] + temp[1];
+			}
+			else if (temp.length == 1) {
+				compare = temp[0];
+			}
+			else
+				compare = "";
+			if (line.charAt(4) != ')')
+				throw new InvalidInputException();	
+		}
+	}
+	/*******
+	 * 
+	 * @param br
+	 * @throws IOException
+	 * @throws MachinePenException
+	 */
+	private void machinePenalties(BufferedReader br) throws IOException, MachinePenException{
+		int i = 0;
+		int j = 0;
+		StringTokenizer st;
+		String line;
+		String numb;
+		try{
+			while(((line = br.readLine()).length() > 0)&&((line.charAt(0)) != ' ')){
+				st = new StringTokenizer(line);
+				j = 0;
+				while (st.hasMoreTokens()){
+					numb = st.nextToken();
+					linePen[i][j] = Integer.parseInt(numb);
+					j++;
+				}
+				if (j <= 7){
+					throw new MachinePenException();
+				}
+				i++;
+			}
+			if (i <= 7){
+				throw new MachinePenException();
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+			throw new MachinePenException();
+		}
+		catch (NumberFormatException e){
+			throw new InvalidPenException();
+		}
+	}
+	/*******
+	 * 
+	 * @param br
+	 * @throws IOException
+	 */
+	private void forbiddenMachine(BufferedReader br) throws IOException{
+		String line;
+		Node current;
+		int i;
+		while (((line = br.readLine()).length() > 0)&&((line.charAt(0)) != ' ')){
+			i = ((line.charAt(1) - 1) %8);
+			if (forbiddenM[i] == null)
+				forbiddenM[i] = new forbiddenNode(line.charAt(1), line.charAt(3));
+			else{
+				current = forbiddenM[i];
+				while (current.getNext() != null){
+					current = current.getNext();
+				}						
+				current.setNext(new forbiddenNode(line.charAt(1), line.charAt(3)));
+			}
+			if (line.charAt(4) != ')')
+				throw new InvalidInputException();	
+		}	
+	}
+	/*******
+	 * 
+	 * @param br
+	 * @throws IOException
+	 */
+	private void tooNearTask(BufferedReader br) throws IOException{
+		String line;
+		int i;
+		Node current;
+		
+		while(((line = br.readLine()).length() > 0)&&((line.charAt(0)) != ' ')){
+			i = (line.charAt(1) - 1)%8;
+			if (tooNearTask[i] == null)
+				tooNearTask[i] = new nearTaskNode(line.charAt(1), line.charAt(3));
+			else{
+				current = tooNearTask[i];
+				while (current.getNext() != null){
+					current = current.getNext();
+				}						
+				current.setNext(new nearTaskNode(line.charAt(1), line.charAt(3)));
+			}
+			if (line.charAt(4) != ')')
+				throw new InvalidInputException();	
+		}
+	}
+	/*******
+	 * 
+	 * @param br
+	 * @throws IOException
+	 */
+	private void tooNearPen(BufferedReader br) throws IOException{
+		String line;
+		String [] parts;
+		String penValue;
+		int value;
+		int i;
+		Node current;
+		try{
+			while(((line = br.readLine()).length() > 0)&&((line.charAt(0)) != ' ')){
+				i = (line.charAt(1) - 1)%8;
+				parts = line.split(",");
+				penValue = parts[2].substring(0, (parts[2].length() - 1));
+				value = Integer.parseInt(penValue);
+				if (tooNearPen[i] == null)
+					tooNearPen[i] = new nearPenNode((parts[0]).charAt(1), (parts[1]).charAt(0), value);
+				else{
+					current = tooNearPen[i];
+					while (current.getNext() != null){
+						current = current.getNext();
+					}						
+					current.setNext(new nearPenNode((parts[0]).charAt(1), (parts[1]).charAt(0), value));
+				}
+			}
+		}catch(NullPointerException e){
+		}catch(NumberFormatException e){
+			throw new InvalidPenException();//Changed from wrong exception, wrongnumbertoonear
+		}		
+	}
+	
+	/*******
+	 * Creates output file
+	 * write output to file and prints to screen
+	 * closes file
+	 * @param output String that is to be written to the output file
+	 * @throws IOException
+	 */
+	private void writeOutput(String output, String outputFile) throws IOException{
+		FileWriter fw = new FileWriter(outputFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(output, 0, output.length());
+		System.out.println(output);
+		bw.close();
+		fw.close();
+	}
+}
--- 449a1/src/TreeTests.java
+++ 449a1/src/TreeTests.java
@@ -1,14 +1,39 @@
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 
-public class TreeTests extends TestCase {
+public class TreeTests{
+	private static Tree tree;
+	private static Node[] fcN;
+	private static Node[] fbN; 
+	private static Node[] tNT; 
+	private static Node[] tNP;
+	private static int[][] mP;
 
-	protected void setUp() throws Exception {
-		super.setUp();
+	@Before
+	public void setUp() throws Exception {
+		fcN = new Node[8];
+		fbN = new Node[8];
+		tNT = new Node[8];
+		tNP = new Node[8];
+		mP = new int[8][8];
 	}
 
+	@After
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		fcN = null;
+		fbN = null;
+		tNT = null;
+		tNP = null;
+		mP = null;
 	}
 
+	@Test
+	public void test()
+	{
+		assertEquals(1,1);
+	}
 }
